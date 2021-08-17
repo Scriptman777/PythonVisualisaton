6a58cd42f86a2b6b8737d87ed9d0f36653bc7f80
Scriptman 777 <dave56938@gmail.com>
2021-08-17 11:00:02 +0200

    Finished folder size visualisation

---
+++ FolderTreeStats/FolderTreeStatsPygal.py
@@ -0,0 +1,48 @@
+import os
+import tkinter as tk
+import pygal
+from pygal.style import DarkStyle
+from tkinter import filedialog
+  
+def get_size(path, subdirs):
+    size = 0
+    for element in os.scandir(path):
+        if element.is_dir() and subdirs:
+            size += get_size(element.path,True)
+        elif element.is_file():
+            size += element.stat().st_size
+
+    return size
+
+def get_subdirs(path):
+    list = []
+    for dir in os.listdir(path):
+        item = path + '/' + dir
+        if os.path.isdir(item):
+            list.append(item)
+    return list
+
+
+
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askdirectory()
+ 
+subdirs = get_subdirs(file_path)
+
+treemap = pygal.Treemap(value_formatter=lambda x: '{:.2f}MB'.format(x), style=DarkStyle)
+
+for dir in subdirs:
+    bottom_dirs = get_subdirs(dir)
+    bot_values = []
+    for bot_dir in bottom_dirs:
+        print(bot_dir)
+        bot_values.append({'value' : get_size(bot_dir,True)/1000000, 'label': bot_dir})
+    treemap.add(os.path.basename(dir),bot_values)
+
+treemap.render_to_file('map.svg')
+    
+
+
