06b411a690b308b5c86bb2480b4bac644443ad38
Scriptman 777 <dave56938@gmail.com>
2021-08-28 15:45:02 +0200

    Added oxygen data to SHealthHoloviews

--- SamsungHealthHoloviews/SamsungHealthHoloviews.py
+++ SamsungHealthHoloviews/SamsungHealthHoloviews.py
@@ -6,11 +6,17 @@ import csv
 
 hv.extension('bokeh')
 
-complete_df = pd.read_csv('heart_rate.csv')
+complete_heart_df = pd.read_csv('heart_rate.csv')
+df_heart = complete_heart_df[['com.samsung.health.heart_rate.start_time','com.samsung.health.heart_rate.heart_rate']]
+df_heart = df_heart.rename(columns={"com.samsung.health.heart_rate.start_time": "Date", "com.samsung.health.heart_rate.heart_rate": "Heart Rate"}).sort_values(by=['Date'])
 
-df = complete_df[['com.samsung.health.heart_rate.start_time','com.samsung.health.heart_rate.heart_rate']]
-df = df.rename(columns={"com.samsung.health.heart_rate.start_time": "Date", "com.samsung.health.heart_rate.heart_rate": "Heart Rate"}).sort_values(by=['Date'])
+complete_oxy_df = pd.read_csv('oxygen.csv')
+df_oxy = complete_oxy_df[['com.samsung.health.oxygen_saturation.start_time','com.samsung.health.oxygen_saturation.spo2']].rename(columns={"com.samsung.health.oxygen_saturation.start_time": "Date", "com.samsung.health.oxygen_saturation.spo2": "O2"}).sort_values(by=['Date'])
 
-graph = hv.Curve(df).opts(width=800, height=500, xrotation=90, color='red')
+#axiswise attribute needed to not contaminate the other axis
+graph_heart = hv.Curve(df_heart).opts(width=800, height=500, xrotation=90, color='red', axiswise=True)
+graph_oxy = hv.Bars(df_oxy).opts(width=800, height=500, xrotation=90, axiswise=True)
 
-hv.save(graph, 'plot.html', backend='bokeh')
+layout = graph_heart + graph_oxy
+
+hv.save(layout, 'plot.html', backend='bokeh')
