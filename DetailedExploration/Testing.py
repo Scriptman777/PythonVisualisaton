c57507d8fd0ea1f27ebb5256605839e04c849fec
Scriptman 777 <dave56938@gmail.com>
2021-11-06 16:10:09 +0100

    Common graph types in thesis

--- DetailedExploration/Testing.py
+++ DetailedExploration/Testing.py
@@ -3,21 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-x1 = [1,2,3,4,5,6,7]
+x1 = ['Pondělí','Úterý','Středa','Čtvrtek','Pátek','Sobota','Neděle']
 y1 = [1,2,3,3.5,2,3,4]
 
 
-fig, ax = plt.subplots(3, figsize =(11, 9))
+fig, ax = plt.subplots(figsize =(11, 9))
 fig.tight_layout(pad=3.0)
 
 
-ax[0].scatter(x1,y1, color="red")
-ax[0].set_title("Bodový graf")
-ax[1].plot(x1,y1, color="red")
-ax[1].set_title("Spojnicový graf")
-ax[2].plot(x1,y1, color="red")
-ax[2].fill_between(x1, y1, color="red", alpha=0.2)
-ax[2].set_title("Plošný graf")
+data = np.random.random(100)
+data = np.append(data,1.5)
+df = pd.DataFrame(data)
+df.boxplot(ax=ax)
+ax.set_title("Krabicový graf")
+ax.grid(b=None)
+
 
 
 plt.show()
