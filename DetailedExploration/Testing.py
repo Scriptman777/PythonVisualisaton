0c5297c78f6564c1eb4f450a7ecfa48067fc722d
Scriptman 777 <dave56938@gmail.com>
2021-11-06 10:26:36 +0100

    Testing - timeseries graphs

--- DetailedExploration/Testing.py
+++ DetailedExploration/Testing.py
@@ -4,19 +4,20 @@ import numpy as np
 
 
 x1 = [1,2,3,4,5,6,7]
-y1 = [1,1,1,1,2,3,4]
-
-x2 = [1,5,6,7]
-y2 = [1,2,3,4]
+y1 = [1,2,3,3.5,2,3,4]
 
 
+fig, ax = plt.subplots(3, figsize =(11, 9))
+fig.tight_layout(pad=3.0)
 
-fig, ax = plt.subplots(2, figsize =(11, 9))
-ax[1].plot(x1,y1, color="green")
-ax[1].plot(x2,y2, color="red")
 
-ax[0].scatter(x2,y2, color="red")
-
+ax[0].scatter(x1,y1, color="red")
+ax[0].set_title("Bodový graf")
+ax[1].plot(x1,y1, color="red")
+ax[1].set_title("Spojnicový graf")
+ax[2].plot(x1,y1, color="red")
+ax[2].fill_between(x1, y1, color="red", alpha=0.2)
+ax[2].set_title("Plošný graf")
 
 
 plt.show()
