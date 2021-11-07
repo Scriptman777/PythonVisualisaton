42ec91bacd93c7a5158ee479d78f1db4abc8846b
Scriptman 777 <dave56938@gmail.com>
2021-11-07 11:04:11 +0100

    Description of the problematic rainbow colormap

--- DetailedExploration/Testing.py
+++ DetailedExploration/Testing.py
@@ -7,16 +7,20 @@ x1 = ['Pondělí','Úterý','Středa','Čtvrtek','Pátek','Sobota','Neděle']
 y1 = [1,2,3,3.5,2,3,4]
 
 
-fig, ax = plt.subplots(figsize =(11, 9))
+fig, ax = plt.subplots(2, figsize =(8, 10))
 fig.tight_layout(pad=3.0)
 
 
-data = np.random.random(100)
-data = np.append(data,1.5)
-df = pd.DataFrame(data)
-df.boxplot(ax=ax)
-ax.set_title("Krabicový graf")
-ax.grid(b=None)
+n = 10000
+x = np.random.standard_normal(n)
+y = x + np.random.standard_normal(n)
+
+ax[0].hexbin(x, y, gridsize=70, cmap='gist_rainbow')
+
+ax[1].hexbin(x, y, gridsize=70, cmap='inferno')
+
+gradient = np.linspace(0, 1, 256)
+gradient = np.vstack((gradient, gradient))
 
 
 
