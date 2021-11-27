ce00f05ce8fe5cb5f5e027e3dcc428de160e4e01
Scriptman 777 <dave56938@gmail.com>
2021-11-27 15:57:54 +0100

    Sound loading and visualization

--- LoadingFiles/LoadPictureFile.py
+++ LoadingFiles/LoadPictureFile.py
@@ -1,36 +1,42 @@
-import PIL.Image
+import PIL
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import ndimage
 
 fig, axs = plt.subplots(2,2,figsize =(8, 8))
 
-
+# Load image
 img = PIL.Image.open('FIM.png')
+# Convert to greyscale
 img_g = img.convert('L')
-array = np.array(img_g)
+
+# Create NumPy array from colored image
+array_rgb = np.array(img)
+# Create NumPy array from greyscale image
+array_g = np.array(img_g)
 
 
 axs[0][0].imshow(img)
 axs[0][0].set_title("Pillow Image")
 
-
+# Resize image
 resized = img.resize((64, 64))
 
 axs[0][1].imshow(resized)
 axs[0][1].set_title("Resized")
 
-
-filtered = ndimage.laplace(array)
+# Laplace filter on array
+filtered = ndimage.laplace(array_g)
 
 axs[1][0].imshow(filtered, cmap='gray')
 axs[1][0].set_title("Laplace filter")
 
-
-sobel = ndimage.sobel(array)
+# Sobel filter on array
+sobel = ndimage.sobel(array_g)
 
 axs[1][1].imshow(sobel, cmap='gray')
 axs[1][1].set_title("Sobel filter")
 
+new_img = PIL.Image.fromarray(filtered)
 
 plt.show()
---
+++ LoadingFiles/LoadSoundFile.py
@@ -0,0 +1,29 @@
+import librosa
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as sciwave
+
+
+fig, axs = plt.subplots(3,1,figsize =(8, 8))
+
+# Load sound from file with sample rate
+audio, sample_rate = librosa.load("sheep.wav")
+# Short time Fourier transform
+stft = librosa.stft(audio)
+# Create spectrogram
+a_to_db = librosa.amplitude_to_db(np.abs(stft), ref=np.max)
+
+# Load with SciPy
+sc_rate, sc_audio = sciwave.read("sheep.wav")
+# X axis for chart
+sc_x = np.linspace(0,len(sc_audio),len(sc_audio))
+
+# Visualize
+librosa.display.waveshow(audio, ax=axs[0])
+axs[0].set_title("Librosa waveshow")
+librosa.display.specshow(a_to_db, ax=axs[1])
+axs[1].set_title("Librosa specshow")
+axs[2].scatter(sc_x, sc_audio, s=0.5)
+axs[2].set_title("SciPy scatter")
+plt.show()
