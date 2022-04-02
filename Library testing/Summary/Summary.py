158cc541218351829bd9b3fa5470d3249fdd4391
Scriptman 777 <dave56938@gmail.com>
2022-04-02 16:00:37 +0200

    Finishing touches 5

--- Library testing/Summary/Summary.py
+++ Library testing/Summary/Summary.py
@@ -3,7 +3,9 @@ import plotly.express as px
 
 df = pandas.read_csv("Summary.csv")
 
-fig = px.bar(df, x="Library", y=["Input", "Output", "Types", "Customisation", "Interactivity"], title="Summary", labels={'value': 'Score'}, color_discrete_sequence=px.colors.qualitative.G10)
+fig = px.bar(df, x="Knihovna", y=["Vstupní formáty", "Výstupní formáty", "Typy grafů", "Možnosti přizpůsobení", "Možnosti interaktivity"], title="Shrnutí", labels={'value': 'Body'}, color_discrete_sequence=px.colors.qualitative.G10)
+fig.update_layout(legend_title="Kategorie hodnocení")
 
 fig.write_image("Summary.svg")
+fig.write_image("Summary.png")
 fig.show()
